ff18203aaf7a88ea7c9490c22a22f93c19f9d343
Swakshan <tsswakshan@gmail.com>
2025-05-15 22:08:22 +0530

    anime(kaa): Added search & filter

--- javascript/anime/src/en/kickassanime.js
+++ javascript/anime/src/en/kickassanime.js
@@ -13,7 +13,7 @@ const mangayomiSources = [
     "hasCloudflare": false,
     "sourceCodeUrl": "",
     "apiUrl": "",
-    "version": "0.0.1",
+    "version": "0.0.3",
     "isManga": false,
     "itemType": 1,
     "isFullData": false,
@@ -58,7 +58,7 @@ class DefaultExtension extends MProvider {
 
     animeList.forEach((anime) => {
       var slug = anime.slug;
-      var posterSlug = anime.poster.hq;
+      var posterSlug = anime.hasOwnProperty("poster") ? anime.poster.hq : "";
 
       var name = anime.hasOwnProperty(titlePref)
         ? anime[titlePref]
@@ -91,19 +91,193 @@ class DefaultExtension extends MProvider {
   }
 
   async search(query, page, filters) {
-    throw new Error("search not implemented");
+    function getCheckBox(state) {
+      var rd = [];
+      state.forEach((item) => {
+        if (item.state) {
+          rd.push(item.value);
+        }
+      });
+      return rd;
+    }
+    function getSelectFilter(filter) {
+      var selectValue = filter.state;
+      var values = filter.values;
+      var selectValue = values[selectValue]["value"];
+      return selectValue;
+    }
+
+    var isFiltersAvailable = !filters || filters.length != 0;
+    var genre = isFiltersAvailable ? getCheckBox(filters[0].state) : [];
+    var year = isFiltersAvailable ? getSelectFilter(filters[1]) : "All";
+    var status = isFiltersAvailable ? getSelectFilter(filters[2]) : "All";
+    var type = isFiltersAvailable ? getSelectFilter(filters[3]) : "All";
+
+    var filt = {};
+    if (genre.length > 0) filt["genre"] = genre;
+    if (year.toLowerCase() != "all") filt["year"] = parseInt(year);
+    if (status.toLowerCase() != "all") filt["status"] = status;
+    if (type.toLowerCase() != "all") filt["type"] = type;
+
+    var filterQuery = this.base64Encode(JSON.stringify(filt));
+
+    var body = {
+      "page": page,
+      "query": query,
+      "filters":filterQuery
+    };
+
+    var baseUrl = this.getBaseUrl();
+    var url = baseUrl + "/api/fsearch";
+    var hdr = this.getHeaders(url);
+    hdr["content-type"] = "application/json";
+
+    var res = await this.client.post(url, hdr, body);
+    var rd = JSON.parse(res.body);
+
+    var list = this.formatList(rd.result);
+    var hasNextPage = rd.maxPage > page;
+    return { list, hasNextPage };
   }
 
   async getDetail(url) {
     throw new Error("getDetail not implemented");
   }
 
-  async getVideoList(url) {
-    throw new Error("getVideoList not implemented");
+  getFilterList() {
+    function formateState(type_name, items, values) {
+      var state = [];
+      for (var i = 0; i < items.length; i++) {
+        state.push({ type_name: type_name, name: items[i], value: values[i] });
+      }
+      return state;
+    }
+
+    var filters = [];
+
+    // Genres
+    var items = [
+      "Action",
+      "Adult Cast",
+      "Adventure",
+      "Anthropomorphic",
+      "Avant Garde",
+      "Award Winning",
+      "Boys Love",
+      "CGDCT",
+      "Childcare",
+      "Combat Sports",
+      "Comedy",
+      "Crossdressing",
+      "Delinquents",
+      "Detective",
+      "Drama",
+      "Ecchi",
+      "Educational",
+      "Erotica",
+      "Fantasy",
+      "Gag Humor",
+      "Girls Love",
+      "Gore",
+      "Gourmet",
+      "Harem",
+      "High Stakes Game",
+      "Historical",
+      "Horror",
+      "Idols (Female)",
+      "Idols (Male)",
+      "Isekai",
+      "Iyashikei",
+      "Josei",
+      "Kids",
+      "Love Polygon",
+      "Magical Sex Shift",
+      "Mahou Shoujo",
+      "Martial Arts",
+      "Mecha",
+      "Medical",
+      "Military",
+      "Music",
+      "Mystery",
+      "Mythology",
+      "Organized Crime",
+      "Otaku Culture",
+      "Parody",
+      "Performing Arts",
+      "Pets",
+      "Psychological",
+      "Racing",
+      "Reincarnation",
+      "Reverse Harem",
+      "Romance",
+      "Romantic Subtext",
+      "Samurai",
+      "School",
+      "Sci-Fi",
+      "Seinen",
+      "Shoujo",
+      "Shounen",
+      "Showbiz",
+      "Slice of Life",
+      "Space",
+      "Sports",
+      "Strategy Game",
+      "Super Power",
+      "Supernatural",
+      "Survival",
+      "Suspense",
+      "Team Sports",
+      "Time Travel",
+      "Vampire",
+      "Video Game",
+      "Visual Arts",
+      "Workplace",
+    ];
+    filters.push({
+      type_name: "GroupFilter",
+      name: "Genres",
+      state: formateState("CheckBox", items, items),
+    });
+
+    // Years
+    const currentYear = new Date().getFullYear();
+    var years = Array.from({ length: currentYear - 1976 }, (_, i) =>
+      (1977 + i).toString()
+    ).reverse();
+    items = ["All", ...years, "1974", "1972", "1971", "1967"];
+
+    filters.push({
+      type_name: "SelectFilter",
+      name: "Years",
+      state: 0,
+      values: formateState("SelectOption", items, items),
+    });
+
+    // Status
+    items = ["All", "Finished", "On going"];
+    var values = ["all", "finished", "airing"];
+    filters.push({
+      type_name: "SelectFilter",
+      name: "Status",
+      state: 0,
+      values: formateState("SelectOption", items, values),
+    });
+
+    // Type
+    items = ["All", "TV", "Movie", "ONA", "OVA", "Special", "TV Special"];
+    values = ["all", "tv", "movie", "ona", "ova", "special", "tv_special"];
+    filters.push({
+      type_name: "SelectFilter",
+      name: "Type",
+      state: 0,
+      values: formateState("SelectOption", items, values),
+    });
+
+    return filters;
   }
 
-  getFilterList() {
-    throw new Error("getFilterList not implemented");
+  async getVideoList(url) {
+    throw new Error("getVideoList not implemented");
   }
 
   getSourcePreferences() {
@@ -129,6 +303,32 @@ class DefaultExtension extends MProvider {
         },
       },
     ];
+  }
+  base64Encode(input) {
+    const chars =
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+    let binary = "";
+
+    // Convert each character to 8-bit binary
+    for (let i = 0; i < input.length; i++) {
+      let bin = input.charCodeAt(i).toString(2);
+      binary += bin.padStart(8, "0");
+    }
+
+    // Split into 6-bit chunks
+    let output = "";
+    for (let i = 0; i < binary.length; i += 6) {
+      let chunk = binary.slice(i, i + 6).padEnd(6, "0");
+      let index = parseInt(chunk, 2);
+      output += chars[index];
+    }
+
+    // Add padding
+    while (output.length % 4 !== 0) {
+      output += "=";
+    }
+
+    return output;
   }
 
   // End
