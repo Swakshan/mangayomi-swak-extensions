bd27d84871b8eb8d161c1eca78f45884dd2aaabc
Swakshan <tsswakshan@gmail.com>
2025-05-30 18:47:07 +0530

    anime(sb): Added popular, latest & search

---
+++ javascript/anime/src/all/streamblasters.js
@@ -0,0 +1,118 @@
+const mangayomiSources = [
+  {
+    "name": "Streamblasters",
+    "id": 1069142360,
+    "baseUrl": "https://www.streamblasters.party",
+    "lang": "all",
+    "typeSource": "multi",
+    "iconUrl":
+      "https://www.streamblasters.party/wp-content/uploads/2022/05/cropped-SBL-Favi-3-192x192.png",
+    "dateFormat": "",
+    "dateFormatLocale": "",
+    "isNsfw": false,
+    "hasCloudflare": false,
+    "sourceCodeUrl": "",
+    "apiUrl": "",
+    "version": "0.0.1",
+    "isManga": false,
+    "itemType": 1,
+    "isFullData": false,
+    "appMinVerReq": "0.5.0",
+    "additionalParams": "",
+    "sourceCodeLanguage": 1,
+    "notes": "",
+    "pkgPath": "anime/src/all/streamblasters.js",
+  },
+];
+class DefaultExtension extends MProvider {
+  constructor() {
+    super();
+    this.client = new Client();
+  }
+
+  getPreference(key) {
+    return new SharedPreferences().get(key);
+  }
+
+  getHeaders(url) {
+    throw new Error("getHeaders not implemented");
+  }
+
+  getBaseUrl() {
+    return this.getPreference("sb_override_base_url");
+  }
+
+  async request(url) {
+    var res = (await this.client.get(url)).body;
+    return new Document(res);
+  }
+
+  async pageList(slug) {
+    var url = this.getBaseUrl() + slug;
+    var doc = await this.request(url);
+
+    var list = [];
+    var items = doc.select(".blog-items.blog-items-control > article");
+    items.forEach((item) => {
+      var imgSet = item.selectFirst("img").attr("srcset").split(", ");
+      var imageUrl = imgSet[imgSet.length - 1].split(" ")[0];
+      var linkSection = item.selectFirst("h3").selectFirst("a");
+      var link = linkSection.getHref;
+      var name = linkSection.text.replace(" Watch Online", "");
+      list.push({
+        name,
+        imageUrl,
+        link,
+      });
+    });
+
+    var hasNextPage = doc.selectFirst(".prev-content").text.length > 0;
+    return { list, hasNextPage };
+  }
+
+  async homePage(page) {
+    var slug = page > 1 ? `/page/${page}/` : ``;
+    return await this.pageList(slug);
+  }
+
+  async getPopular(page) {
+    return await this.homePage(page);
+  }
+
+  async getLatestUpdates(page) {
+    return await this.homePage(page);
+  }
+
+  async search(query, page, filters) {
+    var query = `/?s=${query}`;
+    var slug = page > 1 ? `/page/${page}${query}` : query;
+    return await this.pageList(slug);
+  }
+
+  async getDetail(url) {
+    throw new Error("getDetail not implemented");
+  }
+
+  async getVideoList(url) {
+    throw new Error("getVideoList not implemented");
+  }
+
+  getFilterList() {
+    throw new Error("getFilterList not implemented");
+  }
+
+  getSourcePreferences() {
+    return [
+      {
+        key: "sb_override_base_url",
+        editTextPreference: {
+          title: "Override base url",
+          summary: "Default: https://www.streamblasters.party",
+          value: "https://www.streamblasters.party",
+          dialogTitle: "Override base url",
+          dialogMessage: "",
+        },
+      },
+    ];
+  }
+}
