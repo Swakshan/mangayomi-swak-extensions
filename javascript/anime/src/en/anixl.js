c5231f35d264d3720b4be27871c5e0664e8a24b8
Swakshan <tsswakshan@gmail.com>
2025-09-04 12:11:14 +0530

    anime(anixl): Added details

--- javascript/anime/src/en/anixl.js
+++ javascript/anime/src/en/anixl.js
@@ -13,7 +13,7 @@ const mangayomiSources = [
     "hasCloudflare": false,
     "sourceCodeUrl": "",
     "apiUrl": "",
-    "version": "0.0.1",
+    "version": "0.0.5",
     "isManga": false,
     "itemType": 1,
     "isFullData": false,
@@ -143,8 +143,121 @@ class DefaultExtension extends MProvider {
     return await this.searchList({ keyword:query,page:page});
   }
 
+  async getAnimeDetail(ani_id) {
+    function statusCode(status) {
+      return (
+        {
+          "currently_airing": 0,
+          "finished_airing": 1,
+        }[status] ?? 5
+      );
+    }
+      var query = `query ($ani_id: String!) {
+          get_animesNode(id: $ani_id) {
+            data {
+              info_title
+              info_filmdesc
+              info_meta_genre
+              info_meta_status
+            }
+          }
+        }`;
+    var variables = {
+      "ani_id": ani_id
+    };
+    var res = await this.request(query, variables);
+    var data = res['data']['get_animesNode']['data']
+    var name = data.info_title
+    var description = data.info_filmdesc
+    var genre = data.info_meta_genre
+    var status = statusCode(data.info_meta_status)
+    var link = this.baseUrl+"/title/"+ani_id
+
+    return { name, status, description, genre, link };
+  }
+
+  async getEpisodeDetail(ani_id,page) {
+    var wholeItems = []
+    var query = `query ($ani_id: String!, $page: Int!){
+  get_animesEpisodesList(select:  {
+    ani_id: $ani_id,
+    page: $page
+    size: 150
+  }) {
+    items {
+      data {
+        ep_index
+        ep_title
+        date_create
+        sourcesNode_list {
+          data {
+            src_type
+            sou_id
+          }
+        }
+      }
+    }
+    paging {
+      pages
+    }
+  }
+}`;
+    var variables = {
+      "ani_id": ani_id,
+      "page": page
+    };
+    var res = await this.request(query, variables);
+    var animesEpisodesList = res['data']['get_animesEpisodesList']
+    var items = animesEpisodesList['items']
+
+    items.reverse()
+    if (items.length >0){
+      wholeItems = [...items,...wholeItems]     
+    }
+
+     var maxPage = animesEpisodesList['paging']['pages']
+      if(page<maxPage){
+        return this.getEpisodeDetail(ani_id,page+1)
+      }
+
+      return wholeItems
+  }
+
   async getDetail(url) {
-    throw new Error("getDetail not implemented");
+    var ani_id = url
+    if (url.includes(this.baseUrl)){
+      ani_id = url.split("/title/")[1]
+    }
+    var details = await this.getAnimeDetail(ani_id);
+
+    var epDetails = await this.getEpisodeDetail(ani_id,1)
+    var chapters = [];
+    epDetails.forEach(item=>{
+      var data = item.data
+      var title = `E${data.ep_index}: ${data.ep_title}`
+      var dateUpload = new Date(data.date_create).valueOf().toString()
+      // var link = JSON.stringify(data.sourcesNode_list)
+      var sourcesNode_list = data.sourcesNode_list
+
+      var scanlator = "";
+      var links = {}
+      sourcesNode_list.forEach(node=>{
+        var src_type = node.data.src_type
+        links[src_type] = node.data.sou_id
+        scanlator+=src_type.toUpperCase()+" ";
+      })
+
+      chapters.push({
+        name:title,
+        dateUpload:dateUpload,
+        scanlator:scanlator.trim(),
+        link:JSON.stringify(links)
+      })
+
+    })
+
+    details.chapters = chapters
+    return details
   }
 
   async getVideoList(url) {
