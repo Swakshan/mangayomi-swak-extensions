33eeebff0b960f85fa8d92f0e1c3f3bf2b7fa0da
Swakshan <tsswakshan@gmail.com>
2026-06-06 12:46:21 +0530

    anime(anikoto): Added details

--- javascript/anime/src/all/anikoto.js
+++ javascript/anime/src/all/anikoto.js
@@ -13,7 +13,7 @@ const mangayomiSources = [
     "hasCloudflare": false,
     "sourceCodeUrl": "",
     "apiUrl": "",
-    "version": "0.0.3",
+    "version": "0.0.5",
     "isManga": false,
     "itemType": 1,
     "isFullData": false,
@@ -39,6 +39,7 @@ class DefaultExtension extends MProvider {
       Referer: "https://anikototv.to",
       Origin: "https://anikototv.to",
       "User-Agent": "MangaYomi",
+      "x-requested-with": "XMLHttpRequest",
     };
   }
 
@@ -47,9 +48,21 @@ class DefaultExtension extends MProvider {
   }
 
   async request(slug) {
-    var url = this.getBaseUrl() + slug;
-    var res = await this.client.get(url);
-    return new Document(res.body);
+    var baseUrl = this.getBaseUrl();
+    var hdr = this.getHeaders();
+    var url = slug.includes(baseUrl) ? slug : baseUrl + slug;
+    var res = await this.client.get(url, hdr);
+    return res.body;
+  }
+
+  async requestDoc(slug) {
+    var res = await this.request(slug);
+    return new Document(res);
+  }
+
+  async requestJson(slug) {
+    var res = await this.request(slug);
+    return new Document(JSON.parse(res)["result"]);
   }
 
   async filter({ keyword = "", sort = "default", page = "1" }) {
@@ -57,14 +70,14 @@ class DefaultExtension extends MProvider {
 
     var slug = `/filter?keyword=${keyword}&type=&sort=${sort}&page=${page}`;
 
-    var doc = await this.request(slug);
+    var doc = await this.requestDoc(slug);
 
     var list = [];
     doc
       .selectFirst("#list-items")
       .select(".item")
       .forEach((item) => {
-        var dataId = item.selectFirst(".tip").attr("data-tip")
+        var dataId = item.selectFirst(".tip").attr("data-tip");
         var nameSection = item.selectFirst(".d-title");
         var name =
           titlePref == "e" ? nameSection.text : nameSection.attr("data-jp");
@@ -91,11 +104,76 @@ class DefaultExtension extends MProvider {
   }
 
   async search(query, page, filters) {
-    return await this.filter({ "keyword":query,"sort": "default", "page": page });
+    return await this.filter({
+      "keyword": query,
+      "sort": "default",
+      "page": page,
+    });
   }
 
   async getDetail(url) {
-    throw new Error("getDetail not implemented");
+    function statusCode(status) {
+      return (
+        {
+          "Currently Airing": 0,
+          "Finished Airing": 1,
+        }[status] ?? 5
+      );
+    }
+
+    var urlSplit = url.split("||");
+
+    var link = urlSplit[0];
+    var dataId = urlSplit[1];
+
+    var doc = await this.requestDoc(link);
+
+    var binfo = doc.selectFirst(".binfo");
+    var description = binfo
+      .selectFirst(".synopsis")
+      .selectFirst(".content").text;
+    var genre = [];
+    var bmeta = binfo
+      .selectFirst(".bmeta")
+      .selectFirst(".meta")
+      .select("div")
+      .reverse();
+
+    bmeta[0].select("a").forEach((a) => {
+      genre.push(a.text.trim());
+    });
+
+    var statusText = bmeta[1].selectFirst("a").text.trim();
+    var status = statusCode(statusText);
+
+    var chapters = [];
+    var epBaseUrl = `/ajax/episode/list/${dataId}?vrf=`;
+    doc = await this.requestJson(epBaseUrl);
+    doc.select("li").forEach((item) => {
+      var epTitle = item.attr("title");
+      var a = item.selectFirst("a");
+      var epNum = a.attr("data-num");
+      var episodeTitle = `E${epNum}: ${epTitle}`;
+      var episodeId = a.attr("data-ids");
+
+      var scanlator = "";
+      var dataSub = parseInt(a.attr("data-sub"));
+      var dataDub = parseInt(a.attr("data-dub"));
+      scanlator = dataSub ? "SUB, " : scanlator;
+      scanlator = dataDub ? scanlator + "DUB" : scanlator;
+
+      var dateUpload = a.attr("data-timestamp") + "000";
+
+      chapters.push({
+        name: episodeTitle,
+        url: episodeId,
+        scanlator,
+        dateUpload,
+      });
+    });
+
+    chapters.reverse();
+    return { link, status, description, genre, chapters };
   }
 
   async getVideoList(url) {
